f7f0d5258214c627ab27a761c2574e422e769453
Paul <1323564116@qq.com>
2019-11-15 12:33:55 +0800

    fix travis CI

--- face/ci_dependencies.sh
+++ face/ci_dependencies.sh
@@ -1,12 +1,12 @@
 echo "===SeetaFace2==="
 
 git clone https://github.com/seetafaceengine/SeetaFace2.git ./face/SeetaFace2
-cd ./face/SeetaFace2
-git pull --all
+cd ./face/SeetaFace2 || exit
 git checkout -b develop
+git branch --set-upstream-to=origin/develop develop
 git pull --all
 mkdir build
-cd build
+cd build || exit
 cmake ..
 make && sudo make install
 
