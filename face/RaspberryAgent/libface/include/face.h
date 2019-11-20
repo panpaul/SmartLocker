f602abff507d2252bfc45f621bcc591d4fb71af9
Paul <1323564116@qq.com>
2019-11-20 11:30:11 +0800

    refactor

--- face/RaspberryAgent/libface/include/face.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIBFACE_FACE_H
-#define LIBFACE_FACE_H
-
-char *getFace();
-
-#endif //LIBFACE_FACE_H
---
+++ face/RaspberryAgent/main.cpp
@@ -0,0 +1,69 @@
+#include <opencv2/opencv.hpp>
+#include "UltraFace.h"
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+// init random engine
+default_random_engine randomEngine(time(nullptr));
+// init NCNN engine
+UltraFace ultraface("./model/RFB-320.bin", "./model/RFB-320.param", 320, 2, 0.75); // config model input
+// get VideoCapture instance
+cv::VideoCapture cap(0);
+
+
+string getFace();
+
+int main(int argc, char **argv) {
+
+
+    while (true) {// main loop
+        // getFace
+        string filename = getFace();
+
+        //TODO upload it
+
+        // sleep for 2 seconds
+        Sleep(2000);
+        break;
+    }
+
+    return 0;
+}
+
+string getFace() {
+    // the mat to store camera image
+    cv::Mat frame;
+    // get the frame
+    cap >> frame;
+    // resize to 320*240
+    cv::resize(frame, frame, cv::Size(320, 240), (0, 0), (0, 0), cv::INTER_LINEAR);
+    // convert NCNN mat
+    ncnn::Mat inMat = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR2RGB, frame.cols, frame.rows);
+    // a vector to hold the faces detected
+    std::vector<FaceInfo> face_info;
+
+    // detect the face
+    ultraface.detect(inMat, face_info);
+
+    if (face_info.empty()) { // no faces found
+        return "";
+    }
+
+    if (face_info.size() > 1) { // more than one
+        return "";
+    }
+
+    // now we have only one face
+    auto face = face_info[0];
+    // generate a rect to hold the position
+    cv::Rect rect(face.x1, face.y1, face.x2 - face.x1, face.y2 - face.y1);
+    // prepare the filename
+    std::string filename = "./img/" + std::to_string(randomEngine()) + ".jpg";
+    // save the face into a file
+    cv::imwrite(filename, frame(rect));
+    return filename;
+}
